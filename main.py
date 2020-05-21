39241409982209f1d0e3f9bd6c9461e5f57144c5
satyainturi <satyainturi07@gnail.com>
2020-05-21 19:25:50 +0200

    added multipilcation to master branch

--- main.py
+++ main.py
@@ -42,5 +42,29 @@ def subtraction():
         result = C-D
         return str(float(result))
 
+@app.route('/')
+def index():
+    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+
+
+@app.route('/mul')
+def multiplication():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(value1)
+        D = Fraction(value2)
+        result = C*D
+        return str(float(result))
+        
+
 if __name__ == "__main__":
 	app.run()
