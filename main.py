0000fd6441bbfe6a1496a1bcb70769455bef772e
satyainturi <satyainturi07@gnail.com>
2020-05-21 19:28:11 +0200

    added divison to divi branch

--- main.py
+++ main.py
@@ -1,54 +1,16 @@
 from flask import Flask, request
 from fractions import Fraction
 from decimal import Decimal
+
 app = Flask(__name__)
-@app.route('/')
-def index():
-
-    return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
-@app.route('/add')
-def addition():
-    try:
-        value1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value1='None'
-    try:
-        value2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value2='None'
-    if value1 == 'None' or value2 == 'None' :
-        return 'None'
-    else:
-        C = Fraction(value1)
-        D = Fraction(value2)
-        result = C+D
-        return str(float(result))#removed round function and results in float
-
-@app.route('/sub')
-def subtraction():
-    try:
-        value1=request.args.get('A',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value1='None'
-    try:
-        value2=request.args.get('B',default = 0, type = Fraction)
-    except ZeroDivisionError as error:
-        value2='None'
-    if value1 == 'None' or value2 == 'None' :
-        return 'None'
-    else:
-        C = Fraction(value1)
-        D = Fraction(value2)
-        result = C-D
-        return str(float(result))
 
 @app.route('/')
 def index():
     return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
 
-@app.route('/mul')
-def multiplication():
+@app.route('/div')
+def division():
     try:
         value1=request.args.get('A',default = 0, type = Fraction)
     except ZeroDivisionError as error:
@@ -62,9 +24,12 @@ def multiplication():
     else:
         C = Fraction(value1)
         D = Fraction(value2)
-        result = C*D
-        return str(float(result))
-        
+        try:
+            result = C/D
+            return str(float(result))
+        except ZeroDivisionError as error:
+            return 'None'
+
 
 if __name__ == "__main__":
-	app.run()
+    app.run()
