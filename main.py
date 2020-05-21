b0944708c4a8d01e62ffbf2676160d13c88a1124
satyainturi <satyainturi07@gnail.com>
2020-05-21 19:10:57 +0200

    added addition to master branch

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ from decimal import Decimal
 app = Flask(__name__)
 @app.route('/')
 def index():
+
     return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
+@app.route('/add')
+def addition():
+    try:
+        value1=request.args.get('A',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value1='None'
+    try:
+        value2=request.args.get('B',default = 0, type = Fraction)
+    except ZeroDivisionError as error:
+        value2='None'
+    if value1 == 'None' or value2 == 'None' :
+        return 'None'
+    else:
+        C = Fraction(value1)
+        D = Fraction(value2)
+        result = C+D
+        return str(float(result))#removed round function and results in float
 
 if __name__ == "__main__":
 	app.run()
