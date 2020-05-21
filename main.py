39b8a39e360b7f3e8067a92ea1b901d2b3e19b27
satyainturi <satyainturi07@gnail.com>
2020-05-21 19:05:04 +0200

    stated the assignment

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from flask import Flask, request
-
+from fractions import Fraction
+from decimal import Decimal
 app = Flask(__name__)
-
 @app.route('/')
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
-
-
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
-
+    return 'Usage;\nOperation?A=<Value1>&B=<Value2>\n'
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
