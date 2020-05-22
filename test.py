71dc4fd0320662bd04754124f58a03403e836a82
satyainturi <satyainturi07@gnail.com>
2020-05-22 20:11:06 +0200

    added test script for multiplication on master branch

--- test.py
+++ test.py
@@ -76,7 +76,7 @@ class TestCalculator(unittest.TestCase):#unittest module provides a set of tools
 		solution = self.app.get('add?A=12&B=2/0')
 		self.assertEqual(b"None", solution.data)
 
-def test_sub1(self):
+    def test_sub1(self):
 		#case 1, A is n integer B is an integer
 		solution = self.app.get('/sub?A=6&B=2')
 		self.assertEqual(b'4.0', solution.data)
@@ -141,6 +141,73 @@ def test_sub1(self):
 
 		#case 11, when A input is any number and B=p/q form where q=0
 		solution = self.app.get('sub?A=12&B=2/0')
+		self.assertEqual(b"None", solution.data)
+
+	def test_mul1(self):
+		#case 1, A is n integer B is an integer
+		solution = self.app.get('/mul?A=20&B=2')
+		self.assertEqual(b'40.0', solution.data)
+
+	def test_mul2(self):
+
+		#case 2, A is rational number and B is rational number p/q form
+		solution = self.app.get('/mul?A=3/2&B=22/4')
+		self.assertEqual(b'8.25', solution.data)
+
+	def test_mul3(self):
+
+		#case 3, A is a float and B is a float
+		solution = self.app.get('/mul?A=0.00089&B=100000.00')
+		self.assertEqual(b'89.0', solution.data)
+
+	def test_mul4(self):
+
+		#case 4, when A is float and B is integer
+		solution = self.app.get('/mul?A=22.222&B=98')
+		self.assertEqual(b'2177.756', solution.data)
+
+	def test_mul5(self):
+
+		#case 5, when A is integer and B is float
+		solution = self.app.get('/mul?A=1000&B=9.99')
+		self.assertEqual(b'9990.0', solution.data)
+
+	def test_mul6(self):
+
+		#case 6, when A is fraction p/q and B is an integer
+		solution = self.app.get('/mul?A=11&B=99')
+		self.assertEqual(b'1089.0', solution.data)
+
+	def test_mul7(self):
+
+		#case 7, when A is an integer and B is a fraction p/q
+		solution = self.app.get('/mul?A=23&B=9/13')
+		self.assertEqual(b'15.9230769231', solution.data)
+
+	def test_mul8(self):
+
+		#case 8, when A input is an alphabet(non integer) and B is integer
+		solution = self.app.get('/mul?A=satya&B=22')
+		self.assertEqual(b'0.0', solution.data)#non integer type considered as not valid , in this case which is zero
+
+	def test_mul9(self):
+
+		#case 9, when A input is an integer and B input is an alphabet
+		solution = self.app.get('/mul?A=22&B=reddy')
+		self.assertEqual(b'0.0', solution.data)
+		#when one input is alphabet and other input be any number, whether rational , integer, fraction ultimately the result will be the input which was an integer
+
+	def test_mul10(self):
+
+		#case 10, when A input is of the form p/q where q=0 and B input be any number
+		solution = self.app.get('mul?A=1/0&B=2')
+		self.assertEqual(b"None", solution.data)
+		#according to the script if q=0 in p/q form then it should display an error but it is resolved using zerodivision module
+
+	def test_mul11(self):
+
+		#case 11, when A input is any number and B=p/q form where q=0
+		solution = self.app.get('mul?A=12&B=2/0')
 		self.assertEqual(b"None", solution.data)
 
 
