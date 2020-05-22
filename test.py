fda7812c92c5656c740e1d97619ad9967579e5a7
satyainturi <satyainturi07@gnail.com>
2020-05-22 20:16:36 +0200

    added test script for division on master branch

--- test.py
+++ test.py
@@ -210,6 +210,73 @@ class TestCalculator(unittest.TestCase):#unittest module provides a set of tools
 		solution = self.app.get('mul?A=12&B=2/0')
 		self.assertEqual(b"None", solution.data)
 
+	def test_div1(self):
+		#case 1, A is n integer B is an integer
+		solution = self.app.get('/div?A=20&B=2')
+		self.assertEqual(b'10.0', solution.data)
+
+	def test_div2(self):
+
+		#case 2, A is rational number and B is rational number p/q form
+		solution = self.app.get('/div?A=3/2&B=22/4')
+		self.assertEqual(b'0.272727272727', solution.data)
+
+	def test_div3(self):
+
+		#case 3, A is a float and B is a float
+		solution = self.app.get('/div?A=0.089&B=102.22')
+		self.assertEqual(b'0.000870671101546', solution.data)
+
+	def test_div4(self):
+
+		#case 4, when A is float and B is integer
+		solution = self.app.get('/div?A=22.222&B=98')
+		self.assertEqual(b'0.226755102041', solution.data)
+
+	def test_div5(self):
+
+		#case 5, when A is integer and B is float
+		solution = self.app.get('/div?A=10&B=2.2')
+		self.assertEqual(b'4.54545454545', solution.data)
+
+	def test_div6(self):
+
+		#case 6, when A is fraction p/q and B is an integer
+		solution = self.app.get('/div?A=12&B=100')
+		self.assertEqual(b'0.12', solution.data)
+
+	def test_div7(self):
+
+		#case 7, when A is an integer and B is a fraction p/q
+		solution = self.app.get('/div?A=12&B=2/3')
+		self.assertEqual(b'18.0', solution.data)
+
+	def test_div8(self):
+
+		#case 8, when A input is an alphabet(non integer) and B is integer
+		solution = self.app.get('/div?A=reddy&B=12')
+		self.assertEqual(b'0.0', solution.data)#non integer type considered as not valid , in this case which is zero
+
+	def test_div9(self):
+
+		#case 9, when A input is an integer and B input is an alphabet
+		solution = self.app.get('/div?A=22&B=prakash')
+		self.assertEqual(b'None', solution.data)
+		#when one input is alphabet and other input be any number, whether rational , integer, fraction ultimately the result will be the input which was an integer
+
+	def test_div10(self):
+
+		#case 10, when A input is of the form p/q where q=0 and B input be any number
+		solution = self.app.get('div?A=1/0&B=2')
+		self.assertEqual(b'None', solution.data)
+		#according to the script if q=0 in p/q form then it should display an error
+
+	def test_div11(self):
+
+		#case 11, when A input is any number and B=p/q form where q=0
+		solution = self.app.get('div?A=12&B=2/0')
+		self.assertEqual(b'None', solution.data)
+
 
 if __name__ == '__main__':
 	unittest.main()
